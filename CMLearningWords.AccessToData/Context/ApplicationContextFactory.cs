4134c482e06cfb48bae5a510b8d2ad211a900777
DESKTOP-O46HG7G\bizico105 <oleg.yashchyshy@gmail.com>
2018-07-30 20:01:56 +0300

    rework_on_project_use_as_default_for_tets

--- CMLearningWords.AccessToData/Context/ApplicationContextFactory.cs
+++ CMLearningWords.AccessToData/Context/ApplicationContextFactory.cs
@@ -10,7 +10,7 @@ namespace CMLearningWords.AccessToData.Context
 {
     public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
     {
-        public static string Path { get; } = "";
+        public static string Path { get; } = "Server=28a64aae-6d1d-41d0-835f-a92d0115a5e6.sqlserver.sequelizer.com;Database=db28a64aae6d1d41d0835fa92d0115a5e6;User ID=oiauckdfjbhlakrq;Password=35by4wwfTUcYE8siUycaxLjcBJv3Zh5Ui7wQRJkdUvD2qApZFZfLCsHpT85taCoK;";
         public ApplicationContextFactory() { }
 
         public ApplicationContext CreateDbContext(string[] args)
